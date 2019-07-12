e17863a2c0c0c2e1749fbfad10474a9e88469e7e
Zetsubou Toshio <zetsubou.toshio@gmail.com>
2019-07-12 19:42:52 +0300

    nginx conf

--- src/components/GitHubTool.js
+++ src/components/GitHubTool.js
@@ -1,8 +1,30 @@
 import React from 'react'
+import PropTypes from 'prop-types';
 import {makeStyles} from "@material-ui/core";
 import TextField from "@material-ui/core/TextField";
+import Typography from "@material-ui/core/Typography";
+import AppBar from "@material-ui/core/AppBar";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
+
+function TabContainer(props) {
+    return (
+        <Typography component="div" style={{padding: 8 * 3}}>
+            {props.children}
+        </Typography>
+    );
+}
+
+TabContainer.propTypes = {
+    children: PropTypes.node.isRequired,
+};
+
 
 const useStyles = makeStyles(theme => ({
+    root: {
+        flexGrow: 1,
+        backgroundColor: theme.palette.background.paper,
+    },
     container: {
         display: 'flex',
         flexWrap: 'wrap',
@@ -23,26 +45,40 @@ function GitHubTool() {
     };
 
     const [values, setValues] = React.useState({
-        login: 'xxxxxx'
+        login: 'xxxxxx',
+        tab: "one"
     });
 
 
     return (
-        <div>
-            <div>
-                <TextField
-                    id="standard-name"
-                    label="github login"
-                    className={classes.textField}
-                    value={values.login}
-                    onChange={handleChange('login')}
-                    margin="normal"
-                />
-            </div>
-            <div>
-                <a href={'https://api.github.com/users/' + values.login + '/events/public'}>https://api.github.com/users/{values.login}/events/public</a>
-            </div>
+        <div className={classes.root}>
+            <AppBar position="static">
+                <Tabs value={values.tab} onChange={handleChange("tab")}>
+                    <Tab value="one" label="Информация о пользователе" wrapped/>
+                    <Tab value="two" label="Формирование запроса" wrapped/>
+                </Tabs>
+            </AppBar>
+            {values.tab === 'one' && <TabContainer>
+                <div>
+                    <div>
+                        <TextField
+                            id="standard-name"
+                            label="github login"
+                            className={classes.textField}
+                            value={values.login}
+                            onChange={handleChange('login')}
+                            margin="normal"
+                        />
+                    </div>
+                    <div>
+                        <a href={'https://api.github.com/users/' + values.login + '/events/public'}>https://api.github.com/users/{values.login}/events/public</a>
+                    </div>
+                </div>
+                e</TabContainer>}
+            {values.tab === 'two' && <TabContainer>В разработке</TabContainer>}
         </div>
+
+
     )
 
 
